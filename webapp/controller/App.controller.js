4b6708ac7b594e57b854442b328bfacc46454d1e
Francisco Ríos <frios@deloitte.es>
2021-10-17 23:46:47 +0200

    Season 3 - 7 Oct 2021. Until step 11

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,25 +1,12 @@
 sap.ui.define([
    "sap/ui/demo/walkthrough/base/BaseController",
-   "sap/ui/model/json/JSONModel",
-   "sap/ui/model/resource/ResourceModel",
    "sap/m/MessageToast"
-], function (BaseController, JSONModel, ResourceModel, MessageToast) {
+], function (BaseController, MessageToast) {
    "use strict";
    return BaseController.extend("sap.ui.demo.walkthrough.controller.App", {
 
       onInit: function () {
-         var i18nModel = new ResourceModel({
-            bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
-         });
-         this.setModel(i18nModel, "i18n");
-         // set data model on view
-         var oData = {
-            recipient: {
-               name: "World"
-            }
-         };
-         var oModel = new JSONModel(oData);
-         this.setModel(oModel, 'MiModelo');
+
       },
 
       onShowHello: function () {
